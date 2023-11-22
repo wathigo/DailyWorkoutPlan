30d6bd0023cdc33725b47626cad2f499e6652d56
wathigo <wathigosimon@gmail.com>
2023-11-22 18:07:51 +0300

    Add project set files and configs

---
+++ src/declarations/daily_workout_plan_backend/daily_workout_plan_backend.did.d.ts
@@ -0,0 +1,53 @@
+import type { Principal } from '@dfinity/principal';
+import type { ActorMethod } from '@dfinity/agent';
+
+export type Error = { 'NotFound' : { 'msg' : string } } |
+  { 'Exists' : { 'msg' : string } } |
+  { 'ServerError' : { 'msg' : string } };
+export type Result = { 'Ok' : User } |
+  { 'Err' : Error };
+export type Result_1 = { 'Ok' : WorkoutPlan } |
+  { 'Err' : Error };
+export interface User {
+  'id' : bigint,
+  'age' : bigint,
+  'weight' : bigint,
+  'height' : bigint,
+  'updated_at' : [] | [bigint],
+  'name' : string,
+  'created_at' : bigint,
+}
+export interface UserPayload {
+  'age' : bigint,
+  'weight' : bigint,
+  'height' : bigint,
+  'name' : string,
+}
+export interface WorkoutPlan {
+  'id' : bigint,
+  'updated_at' : [] | [bigint],
+  'push_ups' : bigint,
+  'created_at' : bigint,
+  'user_id' : bigint,
+  'sit_ups' : bigint,
+  'running_time' : bigint,
+}
+export interface WorkoutPlanPayload {
+  'push_ups' : bigint,
+  'user_id' : bigint,
+  'sit_ups' : bigint,
+  'running_time' : bigint,
+}
+export interface _SERVICE {
+  'add_user' : ActorMethod<[UserPayload], [] | [User]>,
+  'delete_user' : ActorMethod<[bigint], Result>,
+  'delete_user_workout_plan' : ActorMethod<[bigint], Result_1>,
+  'generate_workout_plan' : ActorMethod<[bigint], Result_1>,
+  'get_user' : ActorMethod<[bigint], Result>,
+  'get_user_workout_plan' : ActorMethod<[bigint], Result_1>,
+  'update_user' : ActorMethod<[bigint, UserPayload], Result>,
+  'update_user_workout_plan' : ActorMethod<
+    [bigint, WorkoutPlanPayload],
+    Result_1
+  >,
+}
---
+++ src/declarations/daily_workout_plan_backend/daily_workout_plan_backend.did.js
@@ -0,0 +1,54 @@
+export const idlFactory = ({ IDL }) => {
+  const UserPayload = IDL.Record({
+    'age' : IDL.Nat64,
+    'weight' : IDL.Nat64,
+    'height' : IDL.Nat64,
+    'name' : IDL.Text,
+  });
+  const User = IDL.Record({
+    'id' : IDL.Nat64,
+    'age' : IDL.Nat64,
+    'weight' : IDL.Nat64,
+    'height' : IDL.Nat64,
+    'updated_at' : IDL.Opt(IDL.Nat64),
+    'name' : IDL.Text,
+    'created_at' : IDL.Nat64,
+  });
+  const Error = IDL.Variant({
+    'NotFound' : IDL.Record({ 'msg' : IDL.Text }),
+    'Exists' : IDL.Record({ 'msg' : IDL.Text }),
+    'ServerError' : IDL.Record({ 'msg' : IDL.Text }),
+  });
+  const Result = IDL.Variant({ 'Ok' : User, 'Err' : Error });
+  const WorkoutPlan = IDL.Record({
+    'id' : IDL.Nat64,
+    'updated_at' : IDL.Opt(IDL.Nat64),
+    'push_ups' : IDL.Nat64,
+    'created_at' : IDL.Nat64,
+    'user_id' : IDL.Nat64,
+    'sit_ups' : IDL.Nat64,
+    'running_time' : IDL.Nat64,
+  });
+  const Result_1 = IDL.Variant({ 'Ok' : WorkoutPlan, 'Err' : Error });
+  const WorkoutPlanPayload = IDL.Record({
+    'push_ups' : IDL.Nat64,
+    'user_id' : IDL.Nat64,
+    'sit_ups' : IDL.Nat64,
+    'running_time' : IDL.Nat64,
+  });
+  return IDL.Service({
+    'add_user' : IDL.Func([UserPayload], [IDL.Opt(User)], []),
+    'delete_user' : IDL.Func([IDL.Nat64], [Result], []),
+    'delete_user_workout_plan' : IDL.Func([IDL.Nat64], [Result_1], []),
+    'generate_workout_plan' : IDL.Func([IDL.Nat64], [Result_1], []),
+    'get_user' : IDL.Func([IDL.Nat64], [Result], ['query']),
+    'get_user_workout_plan' : IDL.Func([IDL.Nat64], [Result_1], ['query']),
+    'update_user' : IDL.Func([IDL.Nat64, UserPayload], [Result], []),
+    'update_user_workout_plan' : IDL.Func(
+        [IDL.Nat64, WorkoutPlanPayload],
+        [Result_1],
+        [],
+      ),
+  });
+};
+export const init = ({ IDL }) => { return []; };
